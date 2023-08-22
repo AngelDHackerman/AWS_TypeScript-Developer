af068ba540998831afed132f94ada5091211ad35
angelDariaux <angeldariaux@gmail.com>
2023-08-21 21:20:03 -0600

    first challange

---
+++ 06_Advance_TypeScript_Exercises/01-Generic_Types/1.1-genericFunctionToSwapPairs.ts
@@ -0,0 +1,7 @@
+/*
+ * Generic types allow functions, classes, and interfaces to work with multiple types
+ * while retaining type information. They enable the creation of reusable and type-safe
+ * components by using type variables, such as <T>, to stand in for any type. This ensures
+ * flexibility without losing type safety, as the specific type is determined at the time
+ * of invocation or instantiation, and is enforced by the TypeScript compiler.
+ */
