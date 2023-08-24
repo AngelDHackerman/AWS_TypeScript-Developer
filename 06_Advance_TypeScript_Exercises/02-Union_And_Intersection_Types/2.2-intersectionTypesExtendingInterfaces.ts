cef32cb70f5cd775150519608d6824d049635d19
angelDariaux <angeldariaux@gmail.com>
2023-08-23 22:47:49 -0600

    Intersection types solved

---
+++ 06_Advance_TypeScript_Exercises/02-Union_And_Intersection_Types/2.2-intersectionTypesExtendingInterfaces.ts
@@ -0,0 +1,25 @@
+// Exercise Context: Use intersection types to extend interfaces and create a complex object.
+
+interface Person { 
+  name: string;
+  age: number;
+}
+
+interface Employee { 
+  title: string; 
+  department: string;
+}
+
+type PersonEmployee = Person & Employee;
+
+const createPersonEmployee = (person: Person, employee: Employee): PersonEmployee => ({ ...person, ...employee });
+
+// Example Usage remains the same:
+const person: Person = { name: 'John', age: 30 };
+const employee: Employee = { title: 'Manager', department: 'HR' };
+
+const combined = createPersonEmployee(person, employee);
+console.log(combined); // Output: { name: 'John', age: 30, title: 'Manager', department: 'HR' }
+
+
+// Inheritance vs. Combination: extends creates an inheritance relationship, while & simply combines types without creating a relationship.
