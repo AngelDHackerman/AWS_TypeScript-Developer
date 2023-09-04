6110425e2e4f56565d87bd13e39a073c54e9802b
angelDariaux <angeldariaux@gmail.com>
2023-09-03 22:23:56 -0600

    starting the class MyDate

---
+++ TypeScriptAdvancePlatzi/src/01-class.ts
@@ -0,0 +1,26 @@
+const date = new Date();
+date.getHours();
+date.getTime()
+date.toISOString();
+
+const date2 = new Date(1993, 1, 12);
+date2.getHours();
+date2.getTime()
+date2.toISOString();
+
+console.log(date)
+console.log(date2)
+
+let myVar;
+
+class MyDate {
+  year: number;
+  month: number;
+  day: number;
+
+  constructor(year: number, month: number, day: number) {
+    this.year = year;
+    this.month = month;
+    this.day = day
+  }
+}
