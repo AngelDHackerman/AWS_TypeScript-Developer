ead694396176cd3616f49e7a8206fe63d506a873
angelDariaux <angeldariaux@gmail.com>
2023-09-05 20:26:15 -0600

    creando tipado para la respuesta de la API

---
+++ TypeScriptAdvancePlatzi/src/app/models/product.model.ts
@@ -0,0 +1,12 @@
+interface Product {
+  id: number;
+  title: string;
+  price: number;
+  description: string;
+  images: string[];
+  creationAt: string;
+  updatedAt: string;
+  category: Category;
+}
+
+type Products = Product[];
